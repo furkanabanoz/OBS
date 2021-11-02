551660d7c9b485ff77b17db4d2637bd96a4b093f
Furkan <58880808+furkanabanoz@users.noreply.github.com>
2021-11-02 21:31:49 +0300

    Add files via upload

---
+++ obs/src/Teacher.java
@@ -0,0 +1,15 @@
+public class Teacher {
+    String name;
+    String phoneNo;
+    String branch;
+
+    public Teacher(String name, String phoneNo, String branch) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+        this.branch = branch;
+    }
+
+
+
+
+}
